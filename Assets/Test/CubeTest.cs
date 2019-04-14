948f7204f9ebc3747181729491635de22836000f
SimonLiu <liusihui@outlook.com>
2019-04-14 22:25:12 +0800

    2nd Version_NotFinished

---
+++ Assets/Test/CubeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTest : MonoBehaviour
+{
+    public GameObject cube;
+    int width = 10;
+    int height = 10;
+
+
+
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < width; z++)
+                {
+                    Instantiate(cube, new Vector3(x, y, z), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+
+
+}
