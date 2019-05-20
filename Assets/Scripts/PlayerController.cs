7c1e5bfc6214b3317d5b3b2e6e79856f50553c94
SimonLiu <liusihui@outlook.com>
2019-05-20 22:10:40 +0800

    10th Version_InfiniteTerrial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,27 +7,27 @@ public class PlayerController : MonoBehaviour
 
     public GameObject chunkPrefab;
     [SerializeField]
-    private int viewRange;
+    private int viewRange=30;
 
 
     void Start()
     {
-        
+
     }
 
 
     void Update()
     {
-        for (float x = transform.position.x-viewRange; x < transform.position.x+viewRange; x+=ChunkGenerate.width)
+        for (float x = transform.position.x - viewRange; x < transform.position.x + viewRange; x += Chunk.width)
         {
-            for (float z = transform.position.z-viewRange; z < transform.position.z+viewRange; z+=ChunkGenerate.width)
+            for (float z = transform.position.z - viewRange; z < transform.position.z + viewRange; z += Chunk.width)
             {
-                int xx = Mathf.FloorToInt((x / ChunkGenerate.width) * ChunkGenerate.width);
-                int zz = Mathf.FloorToInt((z / ChunkGenerate.width) * ChunkGenerate.width);
-                Chunk chunk = Chunk.GetChunk(Mathf.FloorToInt(xx),0, Mathf.FloorToInt(zz));
+                int xx = Mathf.FloorToInt((x / Chunk.width) * Chunk.width);
+                int zz = Mathf.FloorToInt((z / Chunk.width) * Chunk.width);
+                Chunk chunk = Chunk.GetChunk(Mathf.FloorToInt(xx), 0, Mathf.FloorToInt(zz));
                 if (chunk == null)
                 {
-                    Instantiate(chunkPrefab, new Vector3(Mathf.FloorToInt(xx), 0, Mathf.FloorToInt(zz)), Quaternion.identity);
+                    Instantiate(chunkPrefab, new Vector3(xx, 0, zz), Quaternion.identity);
                 }
             }
         }
